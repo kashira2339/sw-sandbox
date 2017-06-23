5602990c276aa661d177861aedf5195468dac270
Taka Sasaki <tsukune2339@gmail.com>
2017-06-23 20:51:30 +0900

    Update sw.js

--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,8 @@
+const CACHE_VERSION = 'v1'
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('v1').then(cache => cache.addAll([
+    caches.open(CACHE_VERSION).then(cache => cache.addAll([
       '/',
       '/assets/app.js',
       '/assets/style.css',
@@ -13,22 +15,21 @@ self.addEventListener('fetch', event => {
   const cachedResponse = caches.match(event.request)
     .catch(() => fetch(event.request))
     .then(response => {
-      caches.open('v1').then(cache => {
+      caches.open(CACHE_VERSION).then(cache => {
         cache.put(event.request, response);
       })
       return response.clone()
     })
-    .catch(caches.match('/assets/icon.png'))
+    .catch(caches.match('/assets/images/icon.png'))
 
   event.respondWith(cachedResponse)
 })
 
 self.addEventListener('push', event => {
-  console.log(event)
   event.waitUntil(
     self.registration.showNotification('Push Test', {
       body: 'メッセージを受信しました',
-      icon: '../images/icon.png',
+      icon: '/assets/images/icon.png',
       tag: 'push'
     })
   )
